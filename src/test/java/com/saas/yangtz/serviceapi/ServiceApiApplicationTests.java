f3d68642905d32a83584d5a9ed26178d86af8967
chenchao <379757391@qq.com>
2020-11-30 09:41:45 +0800

    Initial commit

---
+++ src/test/java/com/saas/yangtz/serviceapi/ServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saas.yangtz.serviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
